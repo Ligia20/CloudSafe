17869532f5504c42775da69f382690a3397416e4
Arwa Baig <arwabaig786@gmail.com>
2026-08-09 22:25:29 -0400

    making logs page added heading and subheading and first draft of the search bar

---
+++ cloud-safe/src/pages/log.tsx
@@ -0,0 +1,87 @@
+import {
+ IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar, IonSelect, IonSelectOption, IonButton, IonPopover} from '@ionic/react';
+ import React, { useEffect, useState } from 'react';
+
+
+const Log: React.FC = () => {
+const [searchText, setSearchText] = useState("");
+const [selectedSeverity, setSelectedSeverity] = useState('all');
+const [selectedAsset, setSelectedAsset] = useState('all');
+const [isOpen, setIsOpen] = useState(false);
+const [logs, setLogs] = useState([]);
+
+    useEffect(() => {
+            const getLogs = async () => {
+                const response = await fetch ('API ROUTE');
+                const data = await response.json();
+                setLogs(data);
+
+            };
+            getLogs();
+         }, []);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Logs</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent>View and analyze security events and systems logs</IonContent>
+
+      <IonContent>
+        <IonSearchbar
+        //debounce = {} needs to be installed 
+            value={searchText}
+            onIonInput={e => setSearchText(e.detail.value!)} >
+        
+
+        <IonSelect placeholder='All Severities'
+          value={selectedSeverity}
+          onIonChange={e => setSelectedSeverity(e.detail.value)}>
+          <IonSelectOption value='all'>All Severities</IonSelectOption>
+          <IonSelectOption value='critical'>Critical </IonSelectOption>
+          <IonSelectOption value='high'>High</IonSelectOption>
+          <IonSelectOption value='medium'>Medium</IonSelectOption>
+          <IonSelectOption value='low'>Low</IonSelectOption>
+          <IonSelectOption value='info'>Informational</IonSelectOption>
+        </IonSelect>
+
+        <IonSelect placeholder='All Assets'
+          value={selectedAsset}
+          onIonChange={e => setSelectedAsset(e.detail.value)}>
+          <IonSelectOption value='all'>All Assets</IonSelectOption>
+          <IonSelectOption value='web-server'>Web Server </IonSelectOption>
+          <IonSelectOption value='database-server'>Database Server</IonSelectOption>
+          <IonSelectOption value='firewall'>Firewall</IonSelectOption>
+          <IonSelectOption value='vpn-gateway'>VPN Gateway</IonSelectOption>
+          <IonSelectOption value='mail-server'>Mail Server</IonSelectOption>
+        </IonSelect> 
+            {/*LATER PULL FROM DATABASE*/}
+
+
+
+        <IonButton onClick={() => setIsOpen(true)}>Filter</IonButton>
+        <IonPopover
+          isOpen={isOpen}
+          onDidDismiss={() => setIsOpen(false)}
+        >
+          <IonContent>
+            <p>Filter options go here</p>
+          </IonContent>
+        </IonPopover>
+
+
+        </IonSearchbar>
+      </IonContent>
+    </IonPage>
+    
+
+  );
+};
+
+
+
+
+export default Log;
