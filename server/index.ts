eebf93ea3c14b45f57d3b87674d9cab5d6a51201
Isaiah <i.king3458@gmail.com>
2026-08-10 22:06:32 -0400

    cors added

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import express from "express";
+import cors from "cors";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
