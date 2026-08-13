47acdb449248e5d70b4b8ee87a15b9d4d9aff4d3
Isaiah <i.king3458@gmail.com>
2026-08-12 21:58:51 -0400

    Added new pages in fronted to help with security, added verification to better secure /dashbaord, added a /register route and a register page for frontend, added a login.tsx in frontend, password hashed were added, migration tables fixed to reflect current table

--- server/prisma/migrations/20260811145315_add_assets/migration.sql
+++ server/prisma/migrations/20260811145315_add_assets/migration.sql
@@ -1,29 +1,4 @@
 -- CreateTable
-CREATE TABLE "Recent-Alert " (
-    "alert_id" UUID NOT NULL,
-    "severity" TEXT,
-    "alert_name " TEXT,
-    "asset" TEXT,
-    "alert_time" TIMESTAMP(6),
-    "status" TEXT,
-
-    CONSTRAINT "Recent-Alert _pkey" PRIMARY KEY ("alert_id")
-);
-
--- CreateTable
-CREATE TABLE "Recent-Logs" (
-    "log_id" UUID NOT NULL,
-    "asset" TEXT,
-    "source_ip" TEXT,
-    "event" TEXT,
-    "severity" TEXT,
-    "action" TEXT,
-    "log_time" TIMESTAMP(6),
-
-    CONSTRAINT "Recent-Logs_pkey" PRIMARY KEY ("log_id")
-);
-
--- CreateTable
 CREATE TABLE "Asset" (
     "id" UUID NOT NULL,
     "name" TEXT NOT NULL,
